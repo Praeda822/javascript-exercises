237274a125b96c1b2afb425294e3cdfd71ca123c
Patrick Kelly <patdkelly5@gmail.com>
2024-03-14 17:11:55 +1100

    Finished assignments 01-05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...elements) {
+  return array.filter(function (e) {
+    return !elements.includes(e);
+  });
 };
+
+removeFromArray([1, 2, 3, 4], 2, 3); // should remove 2 & 3 and return [1,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+  if (isNaN(a) || isNaN(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  // Ensure a is the smaller number
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+
+  // Calculate the sum using the formula
+  return ((b - a + 1) * (a + b)) / 2;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
